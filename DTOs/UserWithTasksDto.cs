5f0f09846e7da63353c1cfe3384074c40618dc1b
Thanujan2006 <thanuthanujan006@gmail.com>
2026-06-30 16:01:00 +0530

    "firstcommit"

--- DTOs/UserWithTasksDto.cs
+++ DTOs/UserWithTasksDto.cs
@@ -5,5 +5,5 @@ public class UserWithTasksDto
     public int UserId { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-    public List<TaskItemResponseDto> TaskItem { get; set; }
+    public List<TaskItemResponseDto> TaskItem { get; set; } = new();
 }
